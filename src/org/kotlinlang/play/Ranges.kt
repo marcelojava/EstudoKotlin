cb6cad7c2046ca4d7fb29817cefaa7066e88822c
Marcelo <marcelo.iron09@gmail.com>
2019-03-22 20:57:00 -0300

    Estudando ranges no kotlin

---
+++ src/org/kotlinlang/play/Ranges.kt
@@ -0,0 +1,28 @@
+package org.kotlinlang.play
+
+fun main() {
+    for (i in 1..3) {
+        println(i)
+    }
+    println(" ")
+
+    for(i in 2..8 step 2) {
+        println(i)
+    }
+    println(" ")
+
+    for (i in 3 downTo 8) {
+        println(i)
+    }
+    println(" ")
+
+    for (c in 'a'..'z') {
+        println(c)
+    }
+    println(" ")
+
+    for (c in 'z' downTo 's' step 2) {
+        println(c)
+    }
+    println(" ")
+}
