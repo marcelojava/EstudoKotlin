b93e40e0bcfe72b37f45ba595f72b18f9038e94e
Marcelo <marcelo.iron09@gmail.com>
2019-03-22 19:58:20 -0300

    Estudando loops no kotlin

---
+++ src/org/kotlinlang/play/Iterators.kt
@@ -0,0 +1,17 @@
+package org.kotlinlang.play
+
+class Animal(val name : String)
+
+class Zoo(val animals : List<Animal>) {
+        operator fun iterator(): Iterator<Animal> {
+            return animals.iterator()
+        }
+}
+
+fun main() {
+    val zoo = Zoo(listOf(Animal("Macaco"), Animal("leao")))
+
+    for (animal in zoo) {
+        println(animal.name)
+    }
+}
---
+++ src/org/kotlinlang/play/Loops.kt
@@ -0,0 +1,36 @@
+package org.kotlinlang.play
+
+fun main() {
+    //loopFor()
+    //loopWhile()
+    loopDoWhile()
+}
+
+fun loopFor() {
+    val cakes = listOf("carrot", "cheese", "chocolate")
+
+    for (cake in cakes) {
+        println("Que da hora esse bolo de $cake")
+    }
+}
+
+fun loopWhile() {
+    var cakesEaten = 0
+
+    while (cakesEaten < 5) {
+        eatACake()
+        cakesEaten++
+    }
+}
+
+fun loopDoWhile() {
+    var cakesEaten = 0
+
+    do {
+        eatACake()
+        cakesEaten++
+    } while(cakesEaten < 5)
+}
+
+fun eatACake() = println("Eat a Cake")
+
