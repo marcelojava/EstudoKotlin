c604806f1a6575c20652bb946e90d0eaa587441d
Marcelo <marcelo.iron09@gmail.com>
2019-03-22 23:27:26 -0300

    retornando a funcao

--- src/org/kotlinlang/play/functional/Functional.kt
+++ src/org/kotlinlang/play/functional/Functional.kt
@@ -6,6 +6,12 @@ class Calculadora {
     fun multipicar(a : Int, b : Int) =  a * b
 
     fun calcular(a: Int, b : Int, operation: (Int, Int) -> Int) : Int  = operation(a, b)
+
+    fun operacao() : (Int) -> Int {
+        return ::quadrado
+    }
+
+    fun quadrado(x : Int) = x * x
 }
 
 
@@ -13,4 +19,8 @@ fun main() {
     val calc = Calculadora()
     println(calc.calcular(10, 10, calc::soma))
     println(calc.calcular(10, 10, calc::multipicar))
+
+    val func = calc.operacao()
+    println(func(2))
 }
+
