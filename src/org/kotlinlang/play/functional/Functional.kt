c45f49a44bcf8a56b9b96d6ed41051b5720e5544
Marcelo <marcelo.iron09@gmail.com>
2019-03-22 23:14:25 -0300

    ajuste classe calculadora

--- src/org/kotlinlang/play/functional/Functional.kt
+++ src/org/kotlinlang/play/functional/Functional.kt
@@ -5,14 +5,12 @@ class Calculadora {
     fun soma(a : Int, b : Int) = a + b
     fun multipicar(a : Int, b : Int) =  a * b
 
-    fun calcular(a: Int, b : Int, operation: (Int, Int) -> Int) : Int {
-        return operation(a, b)
-    }
+    fun calcular(a: Int, b : Int, operation: (Int, Int) -> Int) : Int  = operation(a, b)
 }
 
 
 fun main() {
     val calc = Calculadora()
     println(calc.calcular(10, 10, calc::soma))
-    println(Calculadora().calcular(10, 10, calc::multipicar))
+    println(calc.calcular(10, 10, calc::multipicar))
 }
